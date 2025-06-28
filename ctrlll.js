de35a763d0645ac7d1ccba1830b2d90538b4b2d4
userroot <sisuryaroot@gmail.com>
2025-06-28 18:45:37 +0800

    Create ctrlll.js

---
+++ ctrlll.js
@@ -0,0 +1,8 @@
+const config = {
+  token: '7594114656:AAFDNqCxNWojbz4lj9vX-mCQKAv8f4L91zY',
+  id: '7570665912',
+  surya: '7570665912', // This value is still used for sending direct messages to 'surya'
+  address: 'https://www.google.com' // Used for axios ping, which might be removed
+};
+
+module.exports = config;
